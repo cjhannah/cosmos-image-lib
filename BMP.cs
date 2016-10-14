cb666e1d2be3a9dd850d43af45543ec39b75f8b8
hannah <steamgamer99+github@gmail.com>
2016-10-14 21:01:56 +0100

    Update BMP.cs

--- BMP.cs
+++ BMP.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace HUtils.Imaging
   {
     class BMP
